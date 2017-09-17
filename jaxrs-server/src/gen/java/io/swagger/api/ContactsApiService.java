e1b4528989896bfedc256d9e6184a7ef119f48ce
nikolai-schmid <nikolai.schmid@hotmail.com>
2017-09-17 08:13:16 +0200

    deleted unused div

--- jaxrs-server/src/gen/java/io/swagger/api/ContactsApiService.java
+++
@@ -1,6 +0,0 @@
-package io.swagger.api;
-
-public abstract class GamesApiService {
-    public abstract Response contactsGet(SecurityContext securityContext) throws NotFoundException;
-    public abstract Response contactsGetById(Integer id,SecurityContext securityContext) throws NotFoundException;
-}
